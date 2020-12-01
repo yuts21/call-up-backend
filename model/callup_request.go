02585e97bc8b5a9beb9093d66fd203550b3cf595
Riptide3 <riptide.yzy@gmail.com>
2020-12-01 14:57:43 +0800

    Add CallupRequest model

---
+++ model/callup_request.go
@@ -0,0 +1,22 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+// CallupRequest 召集令请求模型
+type CallupRequest struct {
+	gorm.Model
+	CallupID    uint
+	Callup      Callup
+	RequesterID uint
+	Requester   User
+	Description string `gorm:"type:text"`
+	Status      uint8
+}
+
+// 状态码
+const (
+	Unprocessed uint = iota + 1
+	Agreed
+	Denied
+	Canceled
+)
