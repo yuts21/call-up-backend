9b1e94645c5b7220005a6a1079d06048768de218
yts1999 <yts1999@yeah.net>
2020-12-19 10:26:21 +0800

    bug fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,7 +11,7 @@ import (
 func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie", "Authorization"}
 	if gin.Mode() == gin.ReleaseMode {
 		// 生产环境需要配置跨域域名，否则403
 		config.AllowOrigins = []string{"http://yts1999.top:2000", "http://www.yts1999.top:2000", "http://39.106.48.217:2000"}
