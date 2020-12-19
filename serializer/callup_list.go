a6f3efbe2e8fb0b06ce634d54a452c994cbf7423
yts1999 <yts1999@yeah.net>
2020-12-19 23:10:16 +0800

    bug fix

--- serializer/callup_list.go
+++ serializer/callup_list.go
@@ -4,24 +4,26 @@ import "call-up/model"
 
 // CallupListItem 召集令列表元素序列化器
 type CallupListItem struct {
-	ID       uint   `json:"id"`
-	Type     uint8  `json:"type"`
-	Name     string `json:"name"`
-	Capacity uint   `json:"cap"`
-	EndDate  int64  `json:"end_date"`
-	Status   uint8  `json:"status"`
+	ID        uint   `json:"id"`
+	Type      uint8  `json:"type"`
+	Name      string `json:"name"`
+	SponsorID uint   `json:"sponsor_id"`
+	Capacity  uint   `json:"cap"`
+	EndDate   int64  `json:"end_date"`
+	Status    uint8  `json:"status"`
 }
 
 func BuildCallupListResponse(callups []model.Callup, total int64) Response {
 	callupList := []CallupListItem{}
 	for _, callup := range callups {
 		callupListItem := CallupListItem{
-			ID: callup.ID,
-			Type: callup.Type,
-			Name: callup.Name,
-			Capacity: callup.Capacity,
-			EndDate: callup.EndDate.Unix(),
-			Status: callup.Status(),
+			ID:        callup.ID,
+			Type:      callup.Type,
+			Name:      callup.Name,
+			SponsorID: callup.SponsorID,
+			Capacity:  callup.Capacity,
+			EndDate:   callup.EndDate.Unix(),
+			Status:    callup.Status(),
 		}
 		callupList = append(callupList, callupListItem)
 	}
